8ce0fae312de64db9846b8893afd59e4c1962043
syw2045 <syw2045@naver.com>
2024-03-01 14:09:31 +0900

    덧칠하기

---
+++ Programmers/코딩테스트 연습/LEVEL1/덧칠하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+    int cur = 0;
+    
+    for(int i=0; i<section.size(); i++){
+        if(cur > section[i]) continue;
+        answer++;
+        cur = section[i] + m;
+    }
+        
+    return answer;
+}
