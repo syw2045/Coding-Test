e723f97c86ab8b533ecb4ee7628889905e567c98
Song Young Wook <81313733+syw2045@users.noreply.github.com>
2024-07-08 20:29:45 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 29.37 ms, Memory: 27.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int k) {
+    vector<int> answer;
+    
+    if(k%2 == 1)
+    {
+        for(int i=0; i<arr.size(); i++){
+            answer.push_back(arr[i] * k);
+        }
+    }
+    else
+    {
+        for(int i=0; i<arr.size(); i++){
+            answer.push_back(arr[i] + k);
+        }
+    }
+    return answer;
+}
