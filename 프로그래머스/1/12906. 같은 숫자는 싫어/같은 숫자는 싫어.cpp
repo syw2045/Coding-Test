d41cf1e8d689601c102abf58e22fc782d85050a0
Song Young Wook <81313733+syw2045@users.noreply.github.com>
2024-07-09 22:28:57 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 108.70 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    
+    arr.erase(unique(arr.begin(), arr.end()), arr.end());
+    
+    for(int i=0; i<arr.size(); i++) answer.push_back(arr[i]);
+    
+    return answer;
+}
